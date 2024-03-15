398c2092164419ab7aeef0b40faaa7fe91bcc9b7
Chenxi <lin.chenxi14@gmail.com>
2024-03-15 18:54:13 -0400

    added benefit component mobile

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from './components/hero/Hero'
+import Benefits from './components/benefits/Benefits';
 
 export default function Home() {
   return (
     <>
       <Hero text={'SEE PRODUCT'} />
+      <Benefits />
     </>
   );
 }
