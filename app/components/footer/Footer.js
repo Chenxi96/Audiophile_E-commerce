36bfd1a27e4589e6e41cad8bbbc27d3e1a17fa25
Chenxi <lin.chenxi14@gmail.com>
2024-03-14 20:05:40 -0400

    added tablet and desktop view of hero component

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -10,26 +10,28 @@ import styles from './Footer.module.scss'
 export default function Footer() {
     return(
         <footer className={styles.footer}>
-            <div className={styles.footer__logo}>
+            <div className={`${styles.footer__logo} ${styles['footer__logo--center']}`}>
                 <Image className={styles.logo__image} src={logo} width={143} height={25}/>
             </div>
             <Nav text={'footerNav__list'} />
-            <p className={styles.footer__description}>
+            <p className={`${styles.footer__description} `}>
             Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
             </p>
-            <p className={styles.footer__copyright}>
-            Copyright 2021. All Rights Reserved
-            </p>
-            <div className={styles.footer__social}>
-                <Link href={'/'}>
-                    <Image src={facebook} />
-                </Link>
-                <Link href={'/'}>
-                    <Image src={twitter} />
-                </Link>
-                <Link href={'/'}>
-                    <Image src={instagram} />
-                </Link>
+            <div className={styles.footer__copyrightSocial}>
+                <p className={`${styles.copyrightSocial__copyright} `}>
+                Copyright 2021. All Rights Reserved
+                </p>
+                <div className={`${styles.copyrightSocial__social}`}>
+                    <Link href={'/'}>
+                        <Image src={facebook} />
+                    </Link>
+                    <Link href={'/'}>
+                        <Image src={twitter} />
+                    </Link>
+                    <Link href={'/'}>
+                        <Image src={instagram} />
+                    </Link>
+                </div>
             </div>
         </footer>
     )
